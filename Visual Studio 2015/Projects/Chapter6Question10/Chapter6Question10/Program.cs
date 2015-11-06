7943463eb654e302a306e0fd3e592bb0776a391d
[Marilee] <[marilee.turscak@gmail.com]>
2015-11-05 20:20:54 -0500

    Add practice problem

---
+++ Visual Studio 2015/Projects/Chapter6Question10/Chapter6Question10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter6Question10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+
+            do
+            {
+                Console.Write("Please enter N (N >= 20 || N <= 0): ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out N) || N >= 20 || N <= 0);
+
+            for (int row = 1; row <= N; row++)
+            {
+                for (int col = row; col <= row + N - 1;)
+                {
+
+                    Console.Write(col + " ");
+                    col++;
+                }
+                Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
+
+
+       // Please enter N(N >= 20 || N <= 0): 5
+//1 2 3 4 5 
+//2 3 4 5 6 
+//3 4 5 6 7 
+//4 5 6 7 8 
+//5 6 7 8 9 
+       
